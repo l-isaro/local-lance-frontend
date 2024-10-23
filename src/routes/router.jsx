7874427ab25fe78ce9c5f312558a85c8c8a55ac6
l-isaro <l.isaro@alustudent.com>
2024-10-23 16:42:12 +0200

    feat: add routing for login page

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import GuestLayout from "../layouts/GuestLayout";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route element={<GuestLayout />}>
         <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />} />
     </Route>
 ))
 
