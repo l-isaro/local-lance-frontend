abc9d0385f9bf659a8e6f1a498704a25f54602cc
l-isaro <l.isaro@alustudent.com>
2024-10-23 15:23:30 +0200

    fix: fix navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="flex gap-36 my-6 items-center rounded-full bg-white py-2 justify-center">
+    <div className="flex gap-36 my-6 items-center rounded-full bg-white py-2 justify-center mx-28">
         <h1 className="text-web-gray text-2xl"><span className="text-web-blue">Local</span>lance</h1>
         <ul className="flex gap-8 items-center text-sm">
             <NavLink to="/" className={({isActive}) => isActive ? "text-web-blue" : ""}>Home</NavLink>
