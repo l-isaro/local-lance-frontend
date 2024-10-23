1a08a1f24849408198cef59bcfb124f94413b34f
l-isaro <l.isaro@alustudent.com>
2024-10-23 18:25:36 +0200

    feat: implement routing for signup page

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter, createRoutesFromElements, Route } from "react-rout
 import GuestLayout from "../layouts/GuestLayout";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import Signup from "../pages/Signup";
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route element={<GuestLayout />}>
         <Route path="/" element={<Home />}/>
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
     </Route>
 ))
 
