b89df39ad46ac19dbb8edd6f919f11777c7fea67
l-isaro <l.isaro@alustudent.com>
2024-10-23 10:09:13 +0200

    feat: style homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,24 @@
+import Button from "../components/Button";
+import remotePicture from "../assets/workingRemotely.png"
+
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <section className="flex items-center">
+        <div>
+          <div className="mb-12">
+            <h2 className="text-5xl mb-6 text-black font-extrabold">Are you looking for Freelancers?</h2>
+            <p className="">
+              Hire Great Freelancers, Fast. Locallance helps you hire elite
+              freelancers at a moment&apos;s notice
+            </p>
+          </div>
+          <div>
+            <Button text="Hire a freelancer" />
+          </div>
+        </div>
+        <img src={remotePicture} alt="" className="w-1/2 h-1/2"/>
+      </section>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ export default {
         "background-green": "#F2FAFA",
         "web-blue": "#1E88E5",
         "web-gray": "#9D9D9D",
+        "web-light-gray": "#252525",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
       },
     },
   },
