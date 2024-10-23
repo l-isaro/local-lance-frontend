19d58a9dbd8cc0c457c724a5cd37bc20760e5209
l-isaro <l.isaro@alustudent.com>
2024-10-23 18:26:28 +0200

    refactor: change page styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,7 @@ export default function Login() {
   return (
     <div>
         <div className="flex gap-4 bg-white flex-col justify-center items-center mx-96 py-10 text-sm">
-            <h1>Welcome</h1>
+            <h1 className="text-2xl">Welcome</h1>
             <p>Please enter your details</p>
             <LoginForm />
             <p className="text-sm">Don&apos;t have an account yet? <NavLink className="text-web-blue">Signup</NavLink></p>
