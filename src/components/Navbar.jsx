0b97ff2a472d9a06b71f45e37fc4ec63812d648c
l-isaro <l.isaro@alustudent.com>
2024-10-23 18:26:08 +0200

    feat: update signup navbar link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="flex gap-36 my-6 items-center rounded-full bg-white py-2 justify-center mx-28">
-        <h1 className="text-web-gray text-2xl"><span className="text-web-blue">Local</span>lance</h1>
+        <NavLink to="/" className="text-web-gray text-2xl"><span className="text-web-blue">Local</span>lance</NavLink>
         <ul className="flex gap-8 items-center text-sm">
             <NavLink to="/" className={({isActive}) => isActive ? "text-web-blue" : ""}>Home</NavLink>
             <NavLink to="/home">Find Work</NavLink>
             <NavLink to="/home">Find Freelancers</NavLink>
             <NavLink to="/login" className={({isActive}) => isActive ? "text-web-blue" : ""}>Log In</NavLink>
-            <NavLink to="/">Sign Up</NavLink>
+            <NavLink to="/signup">Sign Up</NavLink>
             <NavLink to="/" className="bg-web-blue rounded-full py-2 px-8 text-white text-sm ml-7 font-medium">Post a project</NavLink>
         </ul>
         
