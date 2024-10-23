946bfd67f02022ec27c548ee53acdd0a9f0ba46d
l-isaro <l.isaro@alustudent.com>
2024-10-23 10:06:23 +0200

    feat: create navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,17 @@ import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between px-24 py-6">
-        <h1 className="text-web-gray"><span className="text-web-blue">Local</span>lance</h1>
-        <ul className="flex gap-6">
+    <div className="flex gap-36 my-6 items-center rounded-full bg-white py-2 justify-center">
+        <h1 className="text-web-gray text-2xl"><span className="text-web-blue">Local</span>lance</h1>
+        <ul className="flex gap-8 items-center text-sm">
             <NavLink to="/home" className={({isActive}) => isActive ? "text-web-blue" : ""}>Home</NavLink>
             <NavLink to="/home">Find Work</NavLink>
             <NavLink to="/home">Find Freelancers</NavLink>
             <NavLink to="/home">Log In</NavLink>
             <NavLink to="/home">Sign Up</NavLink>
-            
+            <NavLink to="/home" className="bg-web-blue rounded-full py-2 px-8 text-white text-sm ml-7 font-medium">Post a project</NavLink>
         </ul>
-        <NavLink to="/home" className="bg-web-blue rounded-3xl py-2 px-4 text-white">Post a project</NavLink>
+        
     </div>
   )
 }
