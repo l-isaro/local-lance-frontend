9716ec9108c64fd8da6725d4aac965935ad8fc1c
l-isaro <l.isaro@alustudent.com>
2024-10-28 15:48:44 +0200

    feat: style work card component

--- src/components/workCard.jsx
+++ src/components/workCard.jsx
@@ -1,13 +1,13 @@
 import { NavLink } from "react-router-dom";
 
-export default function workCard(props) {
+export default function WorkCard(props) {
   return (
-    <div>
-        <img src={props.img} alt="" />
-        <h3>{props.title}</h3>
-        <p>{props.paragraph}</p>
-        <div>
-            <p>Highest bid ${props.highestBid}</p>
+    <div className="shadow-lg p-4 w-1/4 rounded-lg">
+        <img src={props.img} alt="" className="mx-auto"/>
+        <h3 className="text-center text-black font-medium text-xl mb-1 mt-5">{props.title}</h3>
+        <p className="text-center mb-5">{props.paragraph}</p>
+        <div className="flex gap-4">
+            <p className="font-medium text-gray-500">Highest bid ${props.highestBid}</p>
             <NavLink to="/" className="text-web-blue underline">Apply now</NavLink>
         </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,11 @@ import account from "../assets/account.png";
 import search from "../assets/search.png";
 import apply from "../assets/apply.png";
 import Process from "../components/Process";
-import stats from "../assets/stats.png"
-import MaterialUi from "../assets/MaterialUi.png"
-import CanvaApp from "../assets/CanvaApp"
-import graph from "../assets/graph.png"
+import stats from "../assets/stats.png";
+import MaterialUi from "../assets/MaterialUi.png";
+import CanvaApp from "../assets/CanvaApp.png";
+import graph from "../assets/graph.png";
+import WorkCard from "../components/workCard";
 
 export default function Home() {
   const processes = [
@@ -27,7 +28,40 @@ export default function Home() {
       paragraph: "Apply or save and start your work",
     },
   ];
-  const works = [{img:}]
+  const works = [
+    {
+      img: MaterialUi,
+      title: "Logo Design",
+      paragraph:
+        "Need a professional logo with writing underneath for our jewellery company",
+      highestBid: 500,
+    },
+    {
+      img: CanvaApp,
+      title: "Graphic Design",
+      paragraph:
+        "We need a graphic designer with UI/UX skills for our Furniture company",
+      highestBid: 500,
+    },
+    {
+      img: graph,
+      title: "Need a SEO",
+      paragraph:
+        "Need a SEO for our company who will let our company to a higher level",
+      highestBid: 300,
+    },
+  ];
+
+  const workCards = works.map((work) => (
+    <WorkCard
+      img={work.img}
+      title={work.title}
+      paragraph={work.paragraph}
+      highestBid={work.highestBid}
+      key={work.title}
+    />
+  ));
+
   const processCards = processes.map((process) => (
     <Process
       img={process.img}
@@ -56,22 +90,32 @@ export default function Home() {
         <img src={remotePicture} alt="" className="w-1/2 h-1/2" />
       </section>
       <section className="bg-white py-10 px-28">
-        <div className="flex justify-between rounded-md shadow-xl py-4">{processCards}</div>
+        <div className="flex justify-between rounded-md shadow-xl py-4">
+          {processCards}
+        </div>
       </section>
       <section className="flex px-28 items-center bg-white gap-16 pt-10">
-        <img src={stats} alt="" className="w-2/5"/>
+        <img src={stats} alt="" className="w-2/5" />
         <div>
-            <h2 className="text-5xl text-black mb-8">Find The Best <span className="text-web-blue">Freelancers</span> Here</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut erat bibendum ornare urna, cursus eget convallis. Feugiat imperdiet posuere justo, ultrices interdum
-            sed orci nunc, mattis. Ipsum viverra viverra neque adipiscing arcu, quam dictum. Dui mi viverra dui, sit accumsan, tincidunt massa. Dui cras magnis.</p>
+          <h2 className="text-5xl text-black mb-8">
+            Find The Best <span className="text-web-blue">Freelancers</span>{" "}
+            Here
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut erat
+            bibendum ornare urna, cursus eget convallis. Feugiat imperdiet
+            posuere justo, ultrices interdum sed orci nunc, mattis. Ipsum
+            viverra viverra neque adipiscing arcu, quam dictum. Dui mi viverra
+            dui, sit accumsan, tincidunt massa. Dui cras magnis.
+          </p>
         </div>
       </section>
-      <section>
+      <section className="p-28 bg-white">
         <p>The latest freelance work!</p>
-        <h3>Recently Posted <span>Works</span></h3>
-        <div>
-
-        </div>
+        <h3>
+          Recently Posted <span>Works</span>
+        </h3>
+        <div className="flex gap-10">{workCards}</div>
       </section>
     </div>
   );
