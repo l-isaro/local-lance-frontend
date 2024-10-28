44eb3811e35be98fb48424ef10a2209782f9c788
l-isaro <l.isaro@alustudent.com>
2024-10-28 16:01:48 +0200

    refactor: modify work card component styling

--- src/components/workCard.jsx
+++ src/components/workCard.jsx
@@ -6,8 +6,11 @@ export default function WorkCard(props) {
         <img src={props.img} alt="" className="mx-auto"/>
         <h3 className="text-center text-black font-medium text-xl mb-1 mt-5">{props.title}</h3>
         <p className="text-center mb-5">{props.paragraph}</p>
-        <div className="flex gap-4">
-            <p className="font-medium text-gray-500">Highest bid ${props.highestBid}</p>
+        <div className="flex justify-between">
+          <div className="font-medium text-gray-500">
+            <p>Highest bid </p>
+            <p className="text-center">${props.highestBid}</p>
+            </div>
             <NavLink to="/" className="text-web-blue underline">Apply now</NavLink>
         </div>
     </div>
