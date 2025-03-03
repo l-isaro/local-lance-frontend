40057df8c5ba00d3242e850e844c6fc2499e22a0
l-isaro <l.isaro@alustudent.com>
2025-03-03 11:51:54 +0200

    refactor: remove ApplicationsProvider from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
 import { AuthProvider } from "./contexts/AuthContext";
-import { ApplicationsProvider } from "./contexts/ApplicationsContext";
 
 function App() {
   return (
     <AuthProvider>
-      <ApplicationsProvider>
         <RouterProvider router={router} />
-      </ApplicationsProvider>
     </AuthProvider>
   );
 }
